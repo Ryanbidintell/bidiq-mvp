29ac6449924bbcff42b7462311ba331b6f34ec27
Ryan Elder <ryan@fsikc.com>
2026-02-09 10:37:29 -0600

    Add Node-based server script (Python not installed)

---
+++ start-server-node.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Starting local server on http://localhost:8000
+echo.
+echo Open this URL in your browser:
+echo http://localhost:8000/app.html
+echo.
+echo Press Ctrl+C to stop the server
+echo.
+npx http-server -p 8000
